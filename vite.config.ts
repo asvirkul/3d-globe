eec0c37f8b7f0d64ec0b95674186d64478309d0b
Anton Svrikul <asvirkul@gmail.com>
2026-03-18 22:28:37 +0300

    chore(build): set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/globe-sandbox/' : '/',
+  base: command === 'build' ? '/3d-globe/' : '/',
   plugins: [glsl()],
 }));
